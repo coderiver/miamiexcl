111b1861d8a3568f64f8344ad8003b2abe58f758
moroz <morozarturv@gmail.com>
2014-04-09 12:50:13 +0300

    pushka

--- js/common.js
+++ js/common.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
 
+//lazyload
+var lazy = $('.lazy-load');
+if (lazy.length) {
+  lazy.lazyload({
+    effect : 'fadeIn',
+    delay: 1000,
+    load : function(){
+      $(this).addClass('is-load');
+    }
+  });
+};
+
+
 });
