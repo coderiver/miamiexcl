2b6043ab2ea78e034e78dca126f51eceefaeb707
moroz <morozarturv@gmail.com>
2014-04-09 15:02:10 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -5,12 +5,22 @@ var lazy = $('.lazy-load');
 if (lazy.length) {
   lazy.lazyload({
     effect : 'fadeIn',
-    delay: 1000,
+    //delay: 1000,
     load : function(){
-      $(this).addClass('is-load');
+      //$(this).addClass('is-load');
     }
   });
 };
 
+$(window).scroll(function(){
+	var scroll_top = $(document).scrollTop(),
+			header = $('.header');
+	if (scroll_top > 0) {
+		header.addClass('is-fixed');
+	}
+	else{
+		header.removeClass('is-fixed');
+	}
+});
 
 });
