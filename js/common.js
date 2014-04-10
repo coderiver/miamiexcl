e6e9e2ea624e4713c49aa8152a0f4cbe429c1dd7
moroz <morozarturv@gmail.com>
2014-04-10 15:54:58 +0300

    scrol navigation

--- js/common.js
+++ js/common.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 var header = $('.header'),
 		header_top = header.find('.header__links li.is-center a'),
 		body = $('html, body'),
+		b = $('body'),
     lazy = $('.lazy-load'),
     nav = $('.nav, .pager'),
     page = $('.page');
@@ -18,37 +19,6 @@ if (lazy.length) {
   //});
 };
 
-// nav
-function navigation() {
-	var link = nav.find('a');
-	link.on('click', function(){
-		var item = $(this).attr('href');
-		var top = $('#' + item).offset().top;
-		body.animate({scrollTop: top}, 900);			
-		return false;
-	});	
-}
-navigation();
-
-function navigation_scroll() {
-	var link = nav.find('a');
-	var scroll_top = $(window).scrollTop();
-	page.each(function(){		
-		var item_scroll_top = $(this).offset().top;
-		if (scroll_top >= item_scroll_top) {
-			var item_el = $(this).attr('id');
-			link.each(function(){	
-				var link_item = $(this).attr('href');
-				if (item_el == link_item) {
-					$(this).parents('ul').find('a').removeClass('is-active');
-					$(this).addClass('is-active');
-				};
-			});
-		};
-	});
-};
-navigation_scroll();
-
 // rotate
 function rotate_img() {
 	var scroll_top = $(window).scrollTop(),
@@ -57,26 +27,28 @@ function rotate_img() {
 	el.each(function(){
 		var top = $(this).offset().top,
 				height = $(this).height();
-		if (scroll_top >= top) {
+		if (scroll_top == top) {
 			$(this).parents('.page').addClass('is-active');
 			var thiss = $(this);
 			setTimeout(function(){
 				$('.page.is-active').find('.page__content-in').first().addClass('is-active');
+				$('.page.is-active').find('.page__nav li:first a').addClass('is-active');
 			}, 500);
 		}
 		else{
 			$(this).parents('.page').removeClass('is-active');
 			page.find('.page__content-in').removeClass('is-active');
+			page.find('.page__nav li a').removeClass('is-active');
 		}
 		top = top - height;
 		if (scroll_top >= top) {
 			var angle = grad*(scroll_top - top)/height;
 			angle = grad - angle;
 			if (angle >= 0) {
-				el.find('.page__bg-img').css({'-webkit-transform': 'rotateX('+angle+'deg)', '-moz-transform': 'rotateX('+angle+'deg)', '-o-transform': 'rotateX('+angle+'deg)', '-ms-transform': 'rotateX('+angle+'deg)'});
+				$(this).find('.page__bg-img').css({'-webkit-transform': 'rotateX('+angle+'deg)', '-moz-transform': 'rotateX('+angle+'deg)', '-o-transform': 'rotateX('+angle+'deg)', '-ms-transform': 'rotateX('+angle+'deg)'});
 			}
 			else{
-				el.find('.page__bg-img').css({'-webkit-transform': 'rotateX(0deg)', '-moz-transform': 'rotateX(0deg)', '-o-transform': 'rotateX(0deg)', '-ms-transform': 'rotateX(0deg)'});
+				$(this).find('.page__bg-img').css({'-webkit-transform': 'rotateX(0deg)', '-moz-transform': 'rotateX(0deg)', '-o-transform': 'rotateX(0deg)', '-ms-transform': 'rotateX(0deg)'});
 			}
 		};
 	});
@@ -98,9 +70,83 @@ function tabs() {
 }
 tabs();
 
-$(window).scroll(function(){
-	// nav
-	navigation_scroll();
+
+
+
+
+// clicking nav
+$('.nav a, .pager a').on('click', function() {
+	gotoslide = $(this).data('slide');
+	goto(gotoslide, 500);
+	return false;
+});
+var n = 0;
+function goto(n, time){
+	if(!(b.hasClass('is-running'))){
+
+		//console.log('go');
+
+		b.addClass('is-running');
+		$('.nav a, .pager a').removeClass('is-active');
+		$('.nav li:nth-child('+(n+1)+') a, .pager li:nth-child('+(n+1)+') a').addClass('is-active');
+
+		if(!n){n = 0;}
+
+		slide = $('#slide'+n);
+		if (n == 1) {
+			var el = $('#slide'+n);
+			var t_height = el.height();
+			body.animate({
+		    scrollTop: t_height
+		  }, time);
+		}
+		else {
+			body.animate({
+		    scrollTop: slide.offset().top
+		  }, time);
+		}
+		
+		//hilite nav
+		setTimeout(function(){b.removeClass('is-running');},1000);
+
+	}
+	else{
+		//console.log('cant run');
+	}
+}
+function next(){
+	size = $('.nav li').length;
+	size = size-1;
+	cur = $('.nav a.is-active, .pager a.is-active').data('slide');
+	if(cur<size){
+		goto(cur+1,500)
+	}
+}
+function prev(){
+	cur = $('.nav a.is-active, .pager a.is-active').data('slide');
+	if(cur>0){
+		goto(cur-1,500)
+	}
+}
+prev_u = _.debounce(prev, 60, true)
+next_u = _.debounce(next, 60, true)
+
+var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
+
+$('body').bind(mousewheelevt, function(e){
+	var evt = window.event || e //equalize event object     
+	evt = evt.originalEvent ? evt.originalEvent : evt; //convert to originalEvent if possible               
+	var delta = evt.detail ? evt.detail*(-40) : evt.wheelDelta //check for detail first, because it is used by Opera and FF
+	if(delta > 0) {
+	  prev();
+	}
+	else{
+	  next();
+	}   
+	return false;
+});
+
+$(window).scroll(function(event) {
 	// rotate
 	rotate_img();
 });
