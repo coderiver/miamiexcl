4f32743b07980d343cee63f5e5df8383a094cad1
akella <akella.a@gmail.com>
2014-04-14 00:53:49 +0400

    fix

--- js/common.js
+++ js/common.js
@@ -183,12 +183,23 @@ next_u = _.debounce(next, 160, true)
 var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
 
 $('body').bind(mousewheelevt, function(e){
-
+	if(!isiPad){
+	var evt = window.event || e //equalize event object     
+ 	evt = evt.originalEvent ? evt.originalEvent : evt; //convert to originalEvent if possible               
+ 	var delta = evt.detail ? evt.detail*(-40) : evt.wheelDelta //check for detail first, because it is used by Opera and FF
+ 	if(delta > 0) {
+ 	  prev();
+ 	}
+ 	else{
+ 	  next();
+ 	}   
+ 	return false;
+ 	}
 });
 
 $(window).scroll(function(event) {
 	// rotate
-	//rotate_img();
+	rotate_img();
 });
 
 // slider
